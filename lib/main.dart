4fdf711d3715eae42672a22d91f26fdba14d7e66
zameelabdulsamad <zameelabdulsamad@gmail.com>
2021-08-07 13:59:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Emeen Fresh',
+      theme: ThemeData(
+        primaryColor: bgGrey,
+        scaffoldBackgroundColor: bgWhite,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: primaryText),
+
+      ),
+
+
+      home:Home(),
+    );
+  }
+}
+
+
+
+
